4033f3e1f96e9f3f7b8bc778e70e5ed475b1f8b4
Emrehan Gökçay <101899529+H00wb@users.noreply.github.com>
2025-12-26 13:47:48 +0300

    Create necati-ergin-c-cnts-02-md-answer.c

---
+++ necati-ergin-c-cnts-02-md-answer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main(void)
+{
+	/*
+	Aşağıdaki sabitlerin (constants) türlerini yazınız. Geçersiz olanları belirtiniz.
+	0.F -> 0.0f (float)
+	34f -> invalid type constant, should be 34.f
+	3E3 -> 3*(10^3) = 3000 -> double 
+	2.5e+2 -> 2.5 *(10^2) = 250 -> double
+	45.3L -> Long Double 
+	4.1l -> Long Double
+	.5 -> Double
+	3.2e-2f -> float
+	67F -> invalid constant, should be 67.f
+	*/
+}
