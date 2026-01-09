7e5653b2e0f8dbf73123cb044583c5d4b4c76272
Emrehan Gökçay <101899529+H00wb@users.noreply.github.com>
2026-01-09 16:09:55 +0300

    Fix twoSum function logic for correct index return

--- leetcode-two-sum-brute-force.c
+++ leetcode-two-sum-brute-force.c
@@ -1,19 +1,19 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
 *returnSize = 2;
 int *returnedArray = (int*)malloc(2*sizeof(int));
-for(int i = 0; i<numsSize; ++i)
+for(int i = 0; i<numsSize; i++)
 {
-    if(nums[i] + nums[i+1] == target)
+    for(int j = i+1; j<numsSize; j++)
+    {
+    if(nums[i] + nums[j] == target)
     {
         returnedArray[0] = i;
-        returnedArray[1] = i+1;
-        break;
+        returnedArray[1] = j;
+    }
     }
 }
-return returnedArray;
+ return returnedArray;
 }
+
+
 
