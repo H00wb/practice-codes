24128203fa636aa47187ca75bf9a375bdde44771
Emrehan Gökçay <101899529+H00wb@users.noreply.github.com>
2026-01-21 14:56:18 +0300

    Update necati-ergin-op_02-answer.c

--- Necati Ergin Questions/necati-ergin-op_02-answer.c
+++ Necati Ergin Questions/necati-ergin-op_02-answer.c
@@ -8,7 +8,7 @@ int main(void)
 
 	// x and y variables are initialized as 10 and 20 both respectively.
 
-	int a = x+++y; // Due to maximal munch rule, compiler differentiate operators as  "x ++  + y ;"  and identifier 
+	int a = x+++y; // Due to maximal munch rule, compiler will differentiate operators as  "x ++  + y ;"  and identifier 
 	// a is initialized as 10 + 20 = 30 (due to post increment shows object's original value before incrementation)
 
 	printf("%d\n", -x + y + a);  // -(11)(x is incremented while initializing a) + 20 + 30 = 39
