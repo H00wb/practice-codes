3cee0f59c83f704d2ff54611fdeb7668b990457a
Emrehan Gökçay <101899529+H00wb@users.noreply.github.com>
2026-02-09 18:21:59 +0300

    Update clear-input-buffer-logic.c

--- clear-input-buffer-logic.c
+++ clear-input-buffer-logic.c
@@ -5,6 +5,13 @@ void clear_input_buffer(void) {
     int c;
     while((c = getchar()) != '\n' && c!= EOF) { // "Getchar fonksiyonunun geri dönüş değeri atanan c değişkeninin değeri newline karakteri olmadığı sürece VE c'nin değeri EOF(Input buffer'ın boş olduğunu belirten makro) değilse"
         ; // Aslında burada null statement olmasının sebebi c'nin aslında buffer'da newline karakteri gelene kadar buffer'dan karakter toplayan bir collector gibi çalışmasından başka bir şey istemememiz. Ki bu istenileni de while kontrol deyiminin parantezinde oluşturduk.
+        /* Input 1 : 345emre olsa, 1.scanf 345 karakterlerini extract edip n'in adresindeki değer olarak yazmasından sonra getchar döngü olarak,
+        önce e karakterinin kodunu alır sonra c değişkenine m karakterinin kodu üstüne yazılır sonra r karakterinin kodu üstüne yazılır en son c değişkenine atanan e karakterinin kodu r karakterinin kodunun üstüne yazılır.
+            c = e
+            c = m
+            c = r
+            c = e         gibi. */
+        // Bunu yaparken de buffer'ı temizlemiş olur aslında.
     }
 }
 
